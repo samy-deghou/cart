f3a9358e308d22c6f90f58ffc71cc8fef66c2778
Samy Deghou <deghou@embl.de>
2016-06-24 19:02:10 +0200

    added another unit test for fuzzy name matching

--- test/TestNameMatching.py
+++ test/TestNameMatching.py
@@ -13,13 +13,24 @@ import unittest
 
 class TestNameMatching(unittest.TestCase):
 
-    def testExactMatching(self):
+    def test_exact_matching(self):
         sc = SolrController("",False)
         nmc = NameMatchingController()
         nmc.solrController = sc
         results_json = nmc.submitQuery("ibuprofen", 'name')
         json_object = json.loads(results_json)
         self.assertTrue(json_object['response']['numFound'] > 0)
+
+
+    def test_exact_matching(self):
+        sc = SolrController("",False)
+        nmc = NameMatchingController()
+        nmc.solrController = sc
+        results_json = nmc.submitQuery("ibuprofen", 'name_approx')
+        json_object = json.loads(results_json)
+        self.assertTrue(json_object['response']['numFound'] > 0)
+
+
 
 
 if __name__ == '__main__':
